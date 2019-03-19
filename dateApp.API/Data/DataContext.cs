42952be6b6f4f540bab1d8f384e6d30598698e86
baiju Krishnan <klbaiju@yahoo.co.in>
2019-03-19 11:46:17 +0530

    Initial Commit

---
+++ dateApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using dateApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+namespace dateApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+       public DataContext(DbContextOptions<DataContext>  options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; } 
+    }
+}
